b99bbc9522c78fa17ddaa038358f2a344582a5de
Baelyk <b43lyk@gmail.com>
2025-10-22 18:32:56 -0500

    Workspace flake with Crane

--- flake.nix
+++ flake.nix
@@ -1,18 +1,22 @@
 {
   description = "Baelyk's shell";
 
+  inputs.crane.url = "github:ipetkov/crane";
   inputs.fenix.url = "github:nix-community/fenix";
   inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
   outputs = {
-    self,
+    crane,
     fenix,
     nixpkgs,
+    ...
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-    toolchain = fenix.packages.${system}.stable.toolchain;
+    toolchain = fenix.packages.${system}.default.toolchain;
+    craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+
     # For Iced, https://github.com/iced-rs/iced/blob/master/DEPENDENCIES.md
     dlopenLibraries = with pkgs; [
       libxkbcommon
@@ -20,15 +24,77 @@
       wayland
     ];
     rpath = nixpkgs.lib.makeLibraryPath dlopenLibraries;
+
+    # Crane config for dependencies and workspace members
+    commonArgs = {
+      buildInputs = dlopenLibraries;
+      strictDeps = true;
+      # Only include files relevant to cargo
+      src = craneLib.cleanCargoSource ./.;
+    };
+    # Compiled dependencies
+    cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+    # Helper function to build workspace member bins
+    bin = {
+      name,
+      path,
+      wrapInputs ? [],
+    }: (commonArgs
+      // {
+        inherit cargoArtifacts;
+        # The name of the resulting bin
+        pname = name;
+        # Only build the specified workspace member
+        cargoExtraArgs = "-p ${name}";
+        # Restrict inputs so nonrelevant modifications don't affect building,
+        # e.g. so changes to `notifications` don't mean rebuilding `bar`
+        src = pkgs.lib.fileset.toSource {
+          root = ./.;
+          fileset = pkgs.lib.fileset.unions [
+            ./Cargo.toml
+            ./Cargo.lock
+            (craneLib.fileset.commonCargoSources path)
+          ];
+        };
+        nativeBuildInputs = [
+          # Needed to wrap the resulting bin
+          pkgs.makeWrapper
+        ];
+        postFixup =
+          # Patch the rpath of the resulting bin for Iced dependencies
+          ''
+            echo "Patching rpath..."
+            rpath=$(patchelf --print-rpath $out/bin/${name})
+            patchelf --set-rpath "$rpath:${rpath}" $out/bin/${name}
+          ''
+          # Wrap the resulting bin with runtime dependencies
+          + pkgs.lib.optionalString (wrapInputs != []) ''
+            echo "Wrapping bin..."
+            wrapProgram $out/bin/${name} \
+              --suffix PATH : ${nixpkgs.lib.makeBinPath wrapInputs}
+          '';
+      });
   in {
+    packages.${system} = {
+      bar = craneLib.buildPackage (bin {
+        name = "baelyks-bar";
+        path = ./bar;
+        wrapInputs = [pkgs.sway pkgs.wireplumber];
+      });
+
+      notifications = craneLib.buildPackage (bin {
+        name = "baelyks-notification-daemon";
+        path = ./notifications;
+      });
+    };
+
     devShells.${system}.default = pkgs.mkShell {
       packages = [
         toolchain
-        pkgs.llvmPackages.bintools
       ];
 
-      # For Iced, https://github.com/iced-rs/iced/blob/master/DEPENDENCIES.md
-      env.RUSTFLAGS = "-C link-self-contained=-linker -C link-arg=-Wl,-rpath,${rpath}";
+      # Patch rpath when compiling
+      env.RUSTFLAGS = "-C link-arg=-Wl,-rpath,${rpath}";
 
       shellHook = ''
         echo $(cargo --version)
